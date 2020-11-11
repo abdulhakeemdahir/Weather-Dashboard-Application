fb09430ab29b55b0a099ce894fa573996a2a8160
Abdulhakeem Dahir <somlutions@gmail.com>
2020-11-11 12:09:34 -0800

    testing api connection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log("test")
+// Setting up variables
+var userInput = $("#input").val();
+
+// Setting up url and api key
+
+var APIKey = "f4299bef35c7fb3410eeb230e66758d1";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=" + APIKey;
+
+
+// Testing API connection
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response){
+    console.log(response);
+    console.log(userInput);
+
+});
+
+// Append List of Searches
+
+// Get Current Weather Data and Long Lat
+
+// Get One API Data for UV Index
+
+// Get 5 Day Forcast Data
