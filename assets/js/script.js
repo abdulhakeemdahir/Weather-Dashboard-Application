32603fce01641f643e7d66ef85b033445273c2cf
Abdulhakeem Dahir <somlutions@gmail.com>
2020-11-16 14:00:37 -0800

    setup icon fetching

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 // Setting up variables
 var submitButton = $("#submit");
+var cityDate = $("#city-date");
+var temp = $("#temp");
+var humidity = $("#humidity");
+var windSpeed = $("#wind-speed");
+var uvIndex = $("#uv-index");
+var icon = $("#icon");
+
 
 // Setting up url and api key
 
@@ -13,12 +20,23 @@ function handleAPI(){
 
     var APIKey = "f4299bef35c7fb3410eeb230e66758d1";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
+        var iconCode = response.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+        var weatherIcon = icon.attr("src", iconURL);
+        console.log(iconURL);
+        cityDate.text( response.name + " " + response.date + " " + weatherIcon);
+        temp.text( "Temperature: " + response.main.temp);
+        humidity.text( "Humidity: " + response.main.humidity);
+        windSpeed.text( "Wind Speed: " + response.wind.speed);
+        uvIndex.text( "UV-Index: " + response.main.temp);
         console.log(response);
         console.log(city);
+
 
 });
 }
