96e61f095b1a1738e770bf5ec4675ef28c5e687d
Abdulhakeem Dahir <somlutions@gmail.com>
2020-11-18 12:42:16 -0800

    Setting up One Call API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
+'use strict';
 // Setting up variables
+
+// Main Weather Section
+var contentContainer = $("#content");
 var submitButton = $("#submit");
 var cityDate = $("#city-date");
 var temp = $("#temp");
@@ -7,14 +11,42 @@ var windSpeed = $("#wind-speed");
 var uvIndex = $("#uv-index");
 var icon = $("#icon");
 
-//Setting up API connection
+//5 Day Foercast Section
+var fiveDayContainer = $('<div id="fiveDayWeather" class="card col-sm-2 p-2 bg-primary text-light">');
+var fiveCityDate = $('<p id="five-city-date"">');
+var fiveCityIcon = $('<p id="five-city-icon"">');
+var fiveCityTemp = $('<p id="five-city-temp"">');
+var fiveCityHumidity = $('<p id="five-city-humidity"">');
+
+// Append 5 Day Section
+contentContainer.append(fiveDayContainer);
+fiveDayContainer.append(fiveCityDate, fiveCityIcon, fiveCityTemp, fiveCityHumidity);
+
+// One Call API
+
+function uvIndexHandler (lon, lat){
+    var APIKey = "f4299bef35c7fb3410eeb230e66758d1";
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly&appid=" + APIKey;
+    console.log(queryURL);
+
+    $.ajax({   
+        url: queryURL,
+        method: "GET"
+    }).then(function(response2){
+        uvIndex.text( "UV-Index: " + response2.current.uvi);
+        console.log(response2);
+    });
+}
+
+//Setting up API handler
 
 function handleAPI(){
-    var city = $("#input").val();
+    var city = $("#input").val().trim();
     // Setting up url and api key
     var APIKey = "f4299bef35c7fb3410eeb230e66758d1";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
+   
+    // Current Weather API
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -27,10 +59,14 @@ function handleAPI(){
         temp.text( "Temperature: " + response.main.temp);
         humidity.text( "Humidity: " + response.main.humidity);
         windSpeed.text( "Wind Speed: " + response.wind.speed);
-        uvIndex.text( "UV-Index: " + response.main.temp);
         console.log(response);
         console.log(city);
+        //Get the Longitude and Latitude
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+        console.log(lon,lat);
 
+        uvIndexHandler(lon, lat);
 
 });
 }
